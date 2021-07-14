d533ead982f9e17b6a5c8bd820eb7a19e487035e
Margo-V <flautorfei1929@gmail.com>
2021-07-14 22:16:00 +0400

    Двухсвязный список

---
+++ DataContainers/List2/main.cpp
@@ -0,0 +1,226 @@
+﻿#include <iostream>
+using std::cout;
+using std::cin;
+using std::endl;
+
+#define tab "\t"
+
+class List
+{
+	class Element
+	{
+		int Data;
+		Element* pNext;
+		Element* pPrev;
+	public:
+		Element(int Data, Element* pNext = nullptr, Element* pPrev = nullptr) :
+			Data(Data), pNext(pNext), pPrev(pPrev)
+		{
+			cout << "EConstructor\t" << this << endl;
+		}
+		~Element()
+		{
+			cout << "EDestructor:\t" << this << endl;
+		}
+		friend class List;
+	}*Head, * Tail;
+	size_t size;
+public:
+	size_t get_size()const
+	{
+		return size;
+	}
+	List()
+	{
+		Head = Tail = nullptr;
+		size = 0;
+		cout << "LConstructor:\t" << this << endl;
+	}
+	explicit List(size_t size, int value= int()) :List()
+	{
+		while (size--)push_back(value);
+	}
+	~List()
+	{
+		//while (Head)pop_front();
+		while (Tail)pop_back();
+		cout << "LDestructor:\t" << this << endl;
+	}
+
+	//			Operators:
+	int& operator[](size_t index)
+	{
+		if (index >= size)throw std::exception("Error out of range"); //Выход за пределы диапозона
+		Element* Temp;
+		if (index < size / 2)
+		{
+			Temp = Head;
+			for (int i = 0; i < index; i++)
+				Temp = Temp->pNext;
+		}
+		else
+		{
+			Temp = Tail;
+			for (int i = 0; i < size - index - 1; i++)
+				Temp = Temp->pPrev;
+		}
+		return Temp->Data;
+	}
+
+	//			Adding elements:
+	void push_front(int Data)
+	{
+		if (Head == nullptr && Tail == nullptr)
+		{
+			Head = Tail = new Element(Data);
+			size++;
+			return;
+		}
+		Element* New = new Element(Data);
+		New->pNext = Head;
+		Head->pPrev = New;
+		Head = New;
+		size++;
+	}
+	void push_back(int Data)
+	{
+		if (Head == nullptr && Tail == nullptr)
+		{
+			Head = Tail = new Element(Data);
+			size++;
+			return;
+		}
+		Element* New = new Element(Data);
+		New->pPrev = Tail;
+		Tail->pNext = New;
+		Tail = New;
+		size++;
+	}
+	void insert(size_t index, int Data)
+	{
+		if (index > size)return;
+		if (index == 0)return push_front(Data);
+		if (index == size)return push_back(Data);
+
+		//1) Доходим до позиции, на которую нужно вставить элемент:
+		Element* Temp;
+		if (index < size / 2)
+		{
+			Temp = Head;
+			for (size_t i = 0; i < index; i++)Temp = Temp->pNext;
+		}
+		else
+		{
+			Temp = Tail;
+			for (size_t i = 0; i < size - 1; i++)Temp = Temp->pPrev;
+		}
+		//2) Создаем элемент и включаем его в список:
+		Element* New = new Element(Data);
+		New->pPrev = Temp->pPrev;
+		New->pNext = Temp;
+		Temp->pPrev->pNext = New;
+		Temp->pPrev = New;
+		size++;
+	}
+
+	//			Removing elements:
+	void pop_front()
+	{
+		if (Head == Tail)
+		{
+			delete Head;
+			Head = Tail = nullptr;
+			size--;
+			return;
+		}
+		//1) Исключаем элемент из списка:
+		Head = Head->pNext;
+		//2) Удаляем элемент из памяти:
+		delete Head->pPrev;
+		//3) "Забываем" об удаляемом элементе
+		Head->pPrev = nullptr;
+		size--;
+	}
+	void pop_back()
+	{
+		if (Head == Tail)
+		{
+			delete Tail;
+			Head = Tail = nullptr;
+			size--;
+			return;
+		}
+		//1) Исключаем элемент из списка:
+		Tail = Tail->pPrev;
+		//2) Удаляем исключенный элемент из памяти:
+		delete Tail->pNext;
+		//3)"Затираем" адрес удаленного элемента
+		Tail->pNext = nullptr;
+		size--;
+	}
+	
+	//			Methods:
+	void print()const
+	{
+		for (Element* Temp = Head; Temp; Temp = Temp->pNext)
+		{
+			cout << "Head: " << Head << endl;
+			cout << Temp->pPrev << tab << Temp << tab << Temp->Data << tab <<
+				Temp->pNext << endl;
+			cout << "Количество элементов списка: " << size << endl;
+		}
+	}
+	void reverse_print()const
+	{
+		cout << "Tail: " << Tail << endl;
+		for (Element* Temp = Tail; Temp; Temp = Temp->pPrev)
+		{
+			cout << Temp->pPrev << tab << Temp << tab << Temp->Data << tab <<
+				Temp->pNext << endl;
+			cout << "Количество элементов списка: " << size << endl;
+		}
+	}
+};
+
+//#define BASE_CHECK
+#define SIZE_CONSTRUCTOR_AND_SYBSCRIPT
+
+void main()
+{
+	setlocale(LC_ALL, "ru");
+#ifdef BASE_CHECK
+	int n;
+	cout << "Введите размер списка: "; cin >> n;
+	List list;
+	for (int i = 0; i < n; i++)
+	{
+		//list.push_front(rand() % 100);
+		list.push_back(rand() % 100);
+	}
+
+	size_t index;
+	int value;
+	cout << "Введите индекс добавляемого элемента: "; cin >> index;
+	cout << "Введите значение добавляемого элемента: "; cin >> value;
+	list.insert(index, value);
+
+	list.print();
+	list.reverse_print();
+#endif // BASE_CHECK
+
+	int n;
+	cout << "Введите размер списка: "; cin >> n;
+
+	List list(n);
+
+	for (int i = 0; i < list.get_size(); i++)
+		list[i] = rand() % 100;
+
+	for (int i = 0; i < list.get_size(); i++)
+		cout << list[i] << tab;
+	cout << endl;
+
+	for (int i = list.get_size() - 1; i >= 0; i--)
+		cout << list[i] << tab;
+	cout << endl;
+}
